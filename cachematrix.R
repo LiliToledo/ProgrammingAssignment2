e36d3c39b1264a2caca63295ea33680f846e3f63
LiliToledo <lili.toledo@outlook.com>
2016-12-16 21:16:57 -0600

    New assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inve = NULL 
+  set <- function(y){
+    
+    x <<- y
+    inve <<- NULL
+    
+  }
+  get = function() x
+  setinve = function(inverse) inve <<- inverse
+  getinve = function() inve
+  list(set=set, get=get, setinve=setinve, getinve=getinve)
 
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x, ...){
+  inve = x$getinve()
+  if (!is.null(inve)){
+    message("getting cached information")
+    return(inve)
+  }
+  mat.data = x$get()
+  inve = solve(mat.data, ...)
+  x$setinve(inve)
+  return(inve)
+  }
